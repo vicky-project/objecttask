7404366c33c86afccab96056b6ed39aecd50a3bc
vicky <pratamavr@gmail.com>
2026-02-27 00:44:03 +0800

    updates

--- app/Telegram/TaskCodeCommand.php
+++ app/Telegram/TaskCodeCommand.php
@@ -1,47 +1,54 @@
 <?php
 namespace Modules\ObjectTask\Telegram;
 
+use Modules\ObjectTask\Models\TaskCode;
 use Modules\Telegram\Services\Support\TelegramApi;
 use Modules\Telegram\Services\Handlers\Commands\BaseCommandHandler;
 
 class TaskCodeCommand extends BaseCommandHandler
 {
-	public function __construct(TelegramApi $telegram)
-	{
-		parent::__construct($telegram);
-	}
+  public function __construct(TelegramApi $telegram) {
+    parent::__construct($telegram);
+  }
 
-	public function getName(): string
-	{
-		return "taskcode";
-	}
+  public function getName(): string
+  {
+    return "taskcode";
+  }
 
-	public function getDescription(): string
-	{
-		return "Show task code list";
-	}
+  public function getDescription(): string
+  {
+    return "Show task code list";
+  }
 
-	/*
+  /*
 	 * Handle command
 	 */
-	protected function processCommand(
-		int $chatId,
-		string $text,
-		?string $username = null,
-		array $params = [],
-	): array {
-		try {
-		} catch (\Exception $e) {
-			Log::error("Failed to get task code list", [
-				"message" => $e->getMessage(),
-				"trace" => $e->getTraceAsString(),
-			]);
+  protected function processCommand(
+    int $chatId,
+    string $text,
+    ?string $username = null,
+    array $params = [],
+  ): array {
+    $taskCode = TaskCode::all();
+    $messages = "Task Code:\n\n";
+    foreach ($taskCode as $task) {
+      $messages .= "● {$task->code} - {$task->description}\n";
+    }
 
-			return [
-				"status" => "taskcode_failed",
-				"message" => $e->getMessage(),
-				"send_message" => ["text" => $e->getMessage()],
-			];
-		}
-	}
+    return ['status' => 'taskcode_sent',
+      'send_message' => ['text' => $messages]];
+    try {} catch (\Exception $e) {
+      Log::error("Failed to get task code list", [
+        "message" => $e->getMessage(),
+        "trace" => $e->getTraceAsString(),
+      ]);
+
+      return [
+        "status" => "taskcode_failed",
+        "message" => $e->getMessage(),
+        "send_message" => ["text" => $e->getMessage()],
+      ];
+    }
+  }
 }
