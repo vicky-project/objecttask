88ed7d2d66817c54fa6ce0b827cb6b599332821a
vicky <pratamavr@gmail.com>
2026-02-27 19:08:08 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -134,6 +134,8 @@
     })
     .then(response => response.json())
     .then(data => {
+    console.log(data);
+    alert(JSON.stringify(data));
     renderContents(data);
     })
     .catch(error => {
