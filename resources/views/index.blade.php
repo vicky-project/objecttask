e81d0549a240e5957995d035a6d287ad7b0274f8
vicky <pratamavr@gmail.com>
2026-02-27 19:52:01 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 <div class="main-container">
   <div class="container-custom">
     <div class="page-header">
-      <a href="/" class="home-button" title="Kembali ke Beranda">
+      <a href="{{ config('app.url') }}" class="home-button" title="Kembali ke Beranda">
         <i class="bi bi-house-door"></i>
       </a>
       <h2>Object & Task Code</h2>
