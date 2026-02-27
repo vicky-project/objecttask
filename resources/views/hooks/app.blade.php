1c334b2440c9a1241e31857693c7b7364de41e4e
vicky <pratamavr@gmail.com>
2026-02-27 08:01:34 +0800

    updates

--- resources/views/hooks/app.blade.php
+++ resources/views/hooks/app.blade.php
@@ -1,4 +1,6 @@
-<a href="{{ route('apps.objecttask')}}" class="app-item">
-  <i class="bi bi-journal-code"></i>
-  <span>Financial</span>
-</a>
+<div class="col-4 col-md-2 mb-2">
+  <a href="{{ route('apps.objecttask') }}" class="menu-item rounded-4 p-2" id="dashboard">
+    <i class="bi bi-journal-code"></i>
+    <span>Object Task</span>
+  </a>
+</div>
