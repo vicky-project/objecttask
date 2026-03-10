8aac48bfa3099ff0d1393f9e6e06aa802a9d1cd0
vicky <pratamavr@gmail.com>
2026-03-11 02:42:01 +0800

    updates

--- app/Installations/PostInstallation.php
+++ app/Installations/PostInstallation.php
@@ -6,23 +6,22 @@ use Illuminate\Support\Facades\Artisan;
 
 class PostInstallation
 {
-	public function handle(string $moduleName)
-	{
-		try {
-			$modules = array_merge(["core", "telegram"], [$moduleName]);
-			foreach ($modules as $modulename) {
-				$module = Module::find($modulename);
-				$module->enable();
-			}
+  public function handle(string $moduleName) {
+    try {
+      $modules = array_merge(["coreui", "telegram"], [$moduleName]);
+      foreach ($modules as $modulename) {
+        $module = Module::find($modulename);
+        $module->enable();
+      }
 
-			Artisan::call("migrate", ["--force" => true]);
-		} catch (\Exception $e) {
-			logger()->error(
-				"Failed to run post installation of object task module: " .
-					$e->getMessage(),
-			);
+      Artisan::call("migrate", ["--force" => true]);
+    } catch (\Exception $e) {
+      logger()->error(
+        "Failed to run post installation of object task module: " .
+        $e->getMessage(),
+      );
 
-			throw $e;
-		}
-	}
+      throw $e;
+    }
+  }
 }
