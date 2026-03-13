88a41d1e625c64bfc2044356e6f6bcc06124b9e0
vicky <pratamavr@gmail.com>
2026-03-14 00:40:24 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\ObjectTask\Http\Controllers\ObjectTaskController;
 
 Route::prefix("apps")
-->name("apps.")->middleware("telegram.miniapp")
+->name("apps.")->middleware(["web", "telegram.miniapp"])
 ->group(function () {
   Route::get("objecttask", [ObjectTaskController::class, "index"])->name(
     "objecttask",
