9c306668b68f1795e340c08f9cba6207062763ef
vicky <pratamavr@gmail.com>
2026-04-06 08:37:04 +0800

    updates

--- app/Telegram/CallbackHandler.php
+++ app/Telegram/CallbackHandler.php
@@ -1,7 +1,6 @@
 <?php
 namespace Modules\ObjectTask\Telegram;
 
-use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use Modules\ObjectTask\Services\ObjectCodeService;
 use Modules\Telegram\Services\Support\TelegramApi;
@@ -9,89 +8,93 @@ use Modules\Telegram\Services\Handlers\Callbacks\BaseCallbackHandler;
 
 class CallbackHandler extends BaseCallbackHandler
 {
-	protected ObjectCodeService $objectcodeService;
+  protected ObjectCodeService $objectcodeService;
 
-	public function __construct(
-		TelegramApi $telegramApi,
-		ObjectCodeService $objectcodeService,
-	) {
-		parent::__construct($telegramApi);
-		$this->objectcodeService = $objectcodeService;
-	}
+  public function __construct(
+    TelegramApi $telegramApi,
+    ObjectCodeService $objectcodeService,
+  ) {
+    parent::__construct($telegramApi);
+    $this->objectcodeService = $objectcodeService;
+  }
 
-	public function getModuleName(): string
-	{
-		return "objecttask";
-	}
+  public function getModuleName(): string
+  {
+    return "objecttask";
+  }
 
-	public function getName(): string
-	{
-		return "Object code callback handler";
-	}
+  public function getName(): string
+  {
+    return "Object code callback handler";
+  }
 
-	public function handle(array $data, array $context): array
-	{
-		try {
-			return $this->handleCallbackWithAutoAnswer(
-				$context,
-				$data,
-				fn($data, $context) => $this->processCallback($data, $context),
-			);
-		} catch (\Exception $e) {
-			Log::error("Failed to handle callback of objectcode", [
-				"message" => $e->getMessage(),
-				"trace" => $e->getTraceAsString(),
-			]);
+  public function handle(array $data, array $context): array
+  {
+    try {
+      return $this->handleCallbackWithAutoAnswer(
+        $context,
+        $data,
+        fn($data, $context) => $this->processCallback($data, $context),
+      );
+    } catch (\Exception $e) {
+      Log::error("Failed to handle callback of objectcode", [
+        "message" => $e->getMessage(),
+        "trace" => $e->getTraceAsString(),
+      ]);
 
-			return ["status" => "callback_failed", "answer" => $e->getMessage()];
-		}
-	}
+      return ["status" => "callback_failed",
+        "answer" => $e->getMessage()];
+    }
+  }
 
-	private function processCallback(array $data, array $context): array
-	{
-		try {
-			$entity = $data["entity"];
-			$action = $data["action"];
-			$id = $data["id"] ?? null;
-			$params = $data["params"] ?? [];
+  private function processCallback(array $data, array $context): array
+  {
+    try {
+      $entity = $data["entity"];
+      $action = $data["action"];
+      $id = $data["id"] ?? null;
+      $params = $data["params"] ?? [];
 
-			switch ($entity) {
-				case "object":
-					return $this->handleObject($action, $id, $params);
+      switch ($entity) {
+        case "object":
+          return $this->handleObject($action, $id, $params);
 
-				default:
-					return [];
-			}
-		} catch (\Exception $e) {
-			throw $e;
-		}
-	}
+        default:
+          return [];
+      }
+    } catch (\Exception $e) {
+      throw $e;
+    }
+  }
 
-	private function handleObject(string $action, int $id, array $params): array
-	{
-		switch ($action) {
-			case "content":
-				$contents = $this->objectcodeService->getContentById($id);
-				if (!$contents) {
-					return ["success" => false, "status" => "show_object_content"];
-				}
+  private function handleObject(string $action, int $id, array $params): array
+  {
+    switch ($action) {
+    case "content":
+      $contents = $this->objectcodeService->getContentById($id);
+      if (!$contents) {
+        return ["success" => false,
+          "status" => "show_object_content"];
+      }
 
-				$message = "*{$contents["name"]}*\n\n";
+      $message = "*{$contents["name"]}*\n\n";
 
-				foreach ($contents["contents"] as $content) {
-					$message .= "● `{$content->code}` - {$content->description}\n";
-				}
+      foreach ($contents["contents"] as $content) {
+        $message .= "● `{$content->code}` - {$content->description}\n";
+      }
 
-				$message .= "\n\nnote: _tekan kode untuk menyalin_";
+      $message .= "\n\nnote: _tekan kode untuk menyalin_";
 
-				return [
-					"success" => true,
-					"status" => "show_object_content",
-					"edit_message" => ["text" => $message, "parse_mode" => "MarkdownV2"],
-				];
+      return [
+        "success" => true,
+        "status" => "show_object_content",
+        "edit_message" => ["text" => $message,
+          "parse_mode" => "MarkdownV2"],
+      ];
 
-			default:
-				return ["success" => false, "status" => "no_action_found"];
-		}
-	}
+    default:
+      return ["success" => false,
+        "status" => "no_action_found"];
+    }
+  }
 }
