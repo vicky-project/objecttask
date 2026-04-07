65d9ae758759b6c10c9faba214a85e9ab9081037
vicky <pratamavr@gmail.com>
2026-04-08 00:25:03 +0800

    updates

--- app/Http/Controllers/ObjectTaskController.php
+++ app/Http/Controllers/ObjectTaskController.php
@@ -9,64 +9,54 @@ use Modules\ObjectTask\Services\TaskCodeService;
 
 class ObjectTaskController extends Controller
 {
-	public function __construct(
-		protected TaskCodeService $taskCodes,
-		protected ObjectCodeService $objectCodes,
-	) {
-	}
-	/**
-	 * Display a listing of the resource.
-	 */
-	public function index()
-	{
-		return view("objecttask::index");
-	}
+  public function __construct(
+    protected TaskCodeService $taskCodes,
+    protected ObjectCodeService $objectCodes,
+  ) {}
+  /**
+  * Display a listing of the resource.
+  */
+  public function index() {
+    return view("objecttask::apps");
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 */
-	public function taskCodes()
-	{
-		return response()->json($this->taskCodes->getTaskCodes());
-	}
+  /**
+  * Show the form for creating a new resource.
+  */
+  public function taskCodes() {
+    return response()->json($this->taskCodes->getTaskCodes());
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 */
-	public function categories()
-	{
-		return response()->json($this->objectCodes->getObjectCodes());
-	}
+  /**
+  * Store a newly created resource in storage.
+  */
+  public function categories() {
+    return response()->json($this->objectCodes->getObjectCodes());
+  }
 
-	/**
-	 * Show the specified resource.
-	 */
-	public function contents($id)
-	{
-		return response()->json(
-			$this->objectCodes->getContentById($id)["contents"],
-		);
-	}
+  /**
+  * Show the specified resource.
+  */
+  public function contents($id) {
+    return response()->json(
+      $this->objectCodes->getContentById($id)["contents"],
+    );
+  }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 */
-	public function edit($id)
-	{
-		return view("objecttask::edit");
-	}
+  /**
+  * Show the form for editing the specified resource.
+  */
+  public function edit($id) {
+    return view("objecttask::edit");
+  }
 
-	/**
-	 * Update the specified resource in storage.
-	 */
-	public function update(Request $request, $id)
-	{
-	}
+  /**
+  * Update the specified resource in storage.
+  */
+  public function update(Request $request, $id) {}
 
-	/**
-	 * Remove the specified resource from storage.
-	 */
-	public function destroy($id)
-	{
-	}
+  /**
+  * Remove the specified resource from storage.
+  */
+  public function destroy($id) {}
 }
