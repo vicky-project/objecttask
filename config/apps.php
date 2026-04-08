9a1ab2333f93ca0e759d173b3d7e65fa766273a6
vicky <pratamavr@gmail.com>
2026-04-09 02:34:49 +0800

    updates

--- config/apps.php
+++ config/apps.php
@@ -5,7 +5,6 @@ return [
   'name' => 'Object Task Code',
   'description' => 'Kumpulan Object & Task Code',
   'icon_class' => 'bi bi-journal-code',
-  //'launch_url' => env('APP_URL') . '/apps/objecttask',
   'render_type' => 'iframe',
   'render_config' => [
     'url' => env('APP_URL') . '/apps/objecttask'
